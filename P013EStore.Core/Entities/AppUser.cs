51e6c542b54e4931f2b87051d67d3168cba0b219
utkukemall <utkukemal1999@gmail.com>
2023-05-01 23:04:58 +0300

    Add project files.

---
+++ P013EStore.Core/Entities/AppUser.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P013EStore.Core.Entities
+{
+    public class AppUser : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name ="Ad")]
+        public string Name { get; set; }
+        [Display(Name = "Soyad")]
+        public string? Surname { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
+        public string? UserName { get; set; }
+        [Display(Name = "Telefon")]
+        public string? Phone { get; set; }
+        [Display(Name = "Durum?")]
+        public bool IsActive { get; set; }
+        [Display(Name = "Admin?")]
+        public bool IsAdmin { get; set; }
+        [Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+        [ScaffoldColumn(false)]
+        public Guid? UserGuid { get; set; }
+    }
+}
---
+++ P013EStore.Core/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P013EStore.Core.Entities
+{
+    public class Category : IEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+        [Display(Name = "Açıklama")]
+        public string? Description { get; set; }
+        [Display(Name = "Fotoğraf")]
+        public string? Image { get; set; }
+        [Display(Name = "Durum?")]
+        public bool IsActive { get; set; }
+        [Display(Name = "Üst Menüde Göster")]
+        public bool IsTopMenu { get; set; }
+        [Display(Name = "Üst Kategori")]
+        public int ParentId { get; set; }
+        [Display(Name = "Sıra No")]
+        public int OrderNo { get; set; }
+        [Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+    }
+}
