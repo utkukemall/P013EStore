207800b7eca64b2c8f25cbe230bbd982afd8b6ad
utkukemall <utkukemal1999@gmail.com>
2023-05-08 23:05:40 +0300

    08.05.2023dersi

--- P013EStore.Data/Abstract/IRepository.cs
+++ P013EStore.Data/Abstract/IRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq.Expressions; // Kendi lambda Expression(x=>x.) kullanabileceğimz metotları yazmamızı sağlayan kütüphane. 
 
-
 namespace P013EStore.Data.Abstract
 {
     public interface IRepository<T> where T : class // IRepository interface i dışarıdan alacağı T tipinde bir parametreyle çalışacak ve where şartı ile bu T nin veri tipi bir class olmalıdır dedik.
