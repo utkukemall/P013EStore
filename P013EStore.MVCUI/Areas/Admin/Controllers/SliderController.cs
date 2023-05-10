1a8cd6765f39c9dd4ab25adea78cd866616df864
utkukemall <utkukemal1999@gmail.com>
2023-05-10 21:52:06 +0300

    10.05.2023 dersi

---
+++ P013EStore.MVCUI/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+using P013EStore.Service.Abstract;
+
+namespace P013EStore.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly IService<Slider> _service;
+
+        public SliderController(IService<Slider> service)
+        {
+            _service = service;
+        }
+
+        // GET: SliderController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: SliderController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SliderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SliderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SliderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SliderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SliderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SliderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
