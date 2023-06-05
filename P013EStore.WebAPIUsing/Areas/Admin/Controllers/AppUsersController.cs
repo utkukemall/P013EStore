5cfc0f07afcb5acf1ce9a3851e838a1800641e9f
utkukemall <utkukemal1999@gmail.com>
2023-06-05 21:49:12 +0300

    05.06.2023WebAPI bitti WebApıUsing'e geçildi.

---
+++ P013EStore.WebAPIUsing/Areas/Admin/Controllers/AppUsersController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+
+namespace P013EStore.WebAPIUsing.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Policy = "AdminPolicy")]
+    public class AppUsersController : Controller
+    {
+        private readonly HttpClient _httpClient; // _httpClient nesnesini kullanarak api lere istek gönderebiliriz.
+        private readonly string _apiAdres = "https://localhost:7032/api/AppUsers"; // API adresini web api projesini çalıştırdığımızda adres çubuğundan veya herhangi bir controller a istek atarak Request URL kısmından veya web api projesinde Properties altındaki launchSettings.json kısmından linki bulabiliriz.
+        public AppUsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // GET: AppUsersController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _httpClient.GetFromJsonAsync<List<AppUser>>(_apiAdres); // _httpClient 
+            return View();
+        }
+
+        // GET: AppUsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AppUsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppUsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppUsersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AppUsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AppUsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AppUsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
